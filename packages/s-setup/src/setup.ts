822dd83a52134d3eb6b26214c5df214fc29f24b2
wss <wssgryx@163.com>
2023-05-05 09:53:08 +0800

    feat: show log
    Signed-off-by: wss <wssgryx@163.com>

--- packages/s-setup/src/setup.ts
+++ packages/s-setup/src/setup.ts
@@ -13,58 +13,45 @@ export interface IProps {
   credentials: ICredentials;
 }
 
-/*
-echo "Setup Serverless Devs ing..."
-# 打开调试模式，将命令输出
-set -x
-ls -al
-
-# aliyun cloud authentication infos.
-access_key_id=${{ sts.accessKeyId || "dummy-ak" }}
-access_key_secret=${{ sts.accessKeySecret || "dummy-sk" }}
-security_token=${{ sts.securityToken || "dummy-token" }}
-uid=${{ uid || "dummy-uid" }}
-# account info alias
-alias=my-account
-s --version
-if [[ $? -ne 0 ]]; then
-  echo "Serverless Devs is not installed."
-  exit 1
-fi
-s config add --AccessKeyID "${access_key_id}" --AccessKeySecret "${access_key_secret}" \
---AccountID "${uid}" --SecurityToken "${security_token}" --access "${alias}" -f
-if [[ $? -ne 0 ]]; then
-  echo "Failed to setup Serverless Devs."
-  exit 1
-fi
-echo "Setup Serverless Devs success."
-*/
 export default function setup(props: IProps, logger: Logger) {
   const alias = _.get(props, 'alias', 'default');
   const credentials = _.get(props, 'credentials', {} as ICredentials);
   if (_.isEmpty(credentials)) {
     throw new Error('Failed to setup Serverless Devs');
   }
-  const { status, stdout, stderr } = spawnSync('s --version', { shell: true, encoding: 'utf8' });
-  logger.debug(`Run s --version status: ${status}`);
-  logger.debug(`Run s --version stdout: ${stdout}`);
-  if (stderr) {
-    logger.error(`Run s --version stderr: ${stderr}`);
-  }
-  if (status !== 0) {
-    throw new Error('Serverless Devs is not installed');
+
+  try {
+    checkDevs(logger);
+  } catch (_e) {
+    logger.info('Attempt to install devs...');
+    spawnSync('npm install @serverless-devs/s -g', { shell: true, encoding: 'utf8' });
+    logger.info('Attempt to install devs end. Retry check s');
+    checkDevs(logger);
   }
   let runCommond = `s config add --AccessKeyID "${credentials.accessKeyId}" --AccessKeySecret "${credentials.accessKeySecret}"`;
   if (credentials.accountId) {
-    runCommond = `${runCommond} --AccountID "${credentials.accountId}"`
+    runCommond = `${runCommond} --AccountID ${credentials.accountId}`
   }
   if (credentials.securityToken) {
     runCommond = `${runCommond} --SecurityToken "${credentials.securityToken}"`
   }
-  runCommond = `${runCommond} --access "${alias}" -f`
+  runCommond = `${runCommond} --access "${alias}" -f`;
+  logger.debug(`Run s add commond: ${runCommond}`);
   const addRes = spawnSync(runCommond, { shell: true, encoding: 'utf8' })
-  logger.debug(`Run s add stdout: ${addRes.stdout}`);
+  logger.info(addRes.stdout);
   if (addRes.stderr) {
-    logger.error(`Run s --version stderr: ${addRes.stderr}`);
+    logger.error(addRes.stderr);
+  }
+}
+
+function checkDevs(logger: Logger) {
+  const { status, stdout, stderr } = spawnSync('s --version', { shell: true, encoding: 'utf8' });
+  logger.debug(`Run s --version status: ${status}`);
+  logger.info(stdout);
+  if (stderr) {
+    logger.error(stderr);
+  }
+  if (status !== 0) {
+    throw new Error('Serverless Devs is not installed');
   }
 }
